cdbcbc32fdedc04d41e96c8ddcacf782a3d9b0fc
videhjaiswal9687 <videhjaiswal@gmail.com>
2025-12-21 22:15:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dbConnect from './dbconnect/dbconnect.js';
 import dotenv from 'dotenv';
 import indexRoutes from './routes/indexRoute.js';
 import cors from 'cors';
+import mongoose from "mongoose";
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
@@ -15,8 +16,19 @@ app.use(cors({
   origin: "*", // later you can restrict to your Vercel domain
 }));
 
-//db connection
-dbConnect();
+//database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+   console.log("MongoDB connected successfully");
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("Mongo error:", err.message);
+    process.exit(1);
+  });
 
 
 //routes
